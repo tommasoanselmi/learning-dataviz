042eafc5559c7756d1b84217d1a975dbcf7bcde1
Tommaso Anselmi <ans.tom.ta@gmail.com>
2022-04-20 15:30:23 +0200

    Fill

--- matplotlib-pie.py
+++ matplotlib-pie.py
@@ -3,9 +3,10 @@ from matplotlib import pyplot as plt
 plt.style.use("fivethirtyeight")
 
 slices = [60, 40, 30, 20]
-labels = ['Sixty', 'Forty', 'Thirty', 'Twenty']
+labels = ['Label 1', 'Label 2', 'Label 3', 'Label 3']
+explode = [0, 0, 0.1, 0]
 
-plt.pie(slices, labels=slices, wedgeprops={'edgecolor': 'black'})
+plt.pie(slices, labels=labels, wedgeprops={'edgecolor': 'black'}, explode=explode, startangle=35, autopct='%1.1f%%')
 
 plt.title("Pie")
 plt.tight_layout()
